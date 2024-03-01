ebb1fd23e1d8573951b7bf9faffc578b9cd300b5
Mohsin-mw <null.mmw@gmail.com>
2024-03-02 04:09:44 +0500

    Pref: Removed uuid
    Description:
    removed uuid because it could end up making the size of url much bigger resulting in slower response from the backend

--- server/models/book_model.py
+++ server/models/book_model.py
@@ -2,7 +2,6 @@ from database.db import db
 
 
 class BookModel(db.Model):
-
     __tablename__ = 'books'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -11,7 +10,4 @@ class BookModel(db.Model):
     author = db.Column(db.String(255))
     rating = db.Column(db.Float)
     description = db.Column(db.Text)
-    genres = db.Column(
-        db.String(255))
-
-
+    genres = db.Column(db.String(255))
